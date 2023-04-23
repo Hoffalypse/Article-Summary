af8c3fc3a1aab7d339836a3c20290a2e9d602cd7
Bryan W <bhoff1980@gmail.com>
2023-04-22 22:49:27 -0600

    added hero text and a gradient to make it pop a bit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,24 @@
-
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mt-2 mb-10'>
+        <img src={logo} alt="Summarizer Logo"
+        className='w-28 object-contain'/>
+        <button 
+        type='button' 
+        onClick={() => window.open('https://github.com/Hoffalypse/Article-Summary')}
+        className='black_btn'>
+          GitHub
+        </button>
+
+      </nav>
+      <h1 className='head_text'>
+        Summarize Articles with <br/>
+        <span className='orange_gradient'>OpenAi GPT-4</span>
+      </h1>
+    </header>
   )
 }
 
