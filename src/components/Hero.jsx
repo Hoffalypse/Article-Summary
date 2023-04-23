19762bf03ef4a16a6da68e70353dbb8eadb94369
Bryan W <bhoff1980@gmail.com>
2023-04-23 02:30:54 -0600

    small edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,14 @@ const Hero = () => {
         type='button' 
         onClick={() => window.open('https://github.com/Hoffalypse/Article-Summary')}
         className='black_btn'>
-          GitHub
+          Open Source
         </button>
 
       </nav>
       <h1 className='head_text'>
         Summarize Articles with <br
         className='max-md:hidden'/>
-        <span className='orange_gradient'>OpenAi GPT-4</span>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with HoffNotes, an open-source article summarizer that transforms lengthy articles into clear and concise summaries. 
