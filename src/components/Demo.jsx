2d11542bb024519ccf488f2a87497b5b76a9eaf4
Bryan W <bhoff1980@gmail.com>
2023-04-23 01:36:02 -0600

    app is functioning but considering removing redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
+
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
@@ -9,8 +10,9 @@ const Demo = () => {
     summary: "",
   });
   const [allArticles, setAllArticles] = useState([]);
-
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const [copied, setCopied] = useState("");
+
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
       localStorage.getItem("articles")
@@ -23,16 +25,31 @@ const Demo = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+
+    if (existingArticle) return setArticle(existingArticle);
+
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      const updateArticles = [newArticle, ...allArticles];
-      setAllArticles(updateArticles);
-      setArticle(newArticle);
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-      localStorage.setItem("articles", JSON.stringify(updateArticles));
+      // update state and local storage
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000)
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
@@ -68,19 +85,48 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className="link_card"
             >
-              <div className="copy_btn">
+              <div className="copy_btn" onClick={() => handleCopy(item.url) }>
                 <img
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt="copy icon"
                   className="w-[40%] h-[40%] onject-contain"
                 />
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium, text-sm truncate">{item.url}</p>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium, text-sm truncate">
+                {item.url}
+              </p>
             </div>
           ))}
         </div>
       </div>
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that was not supposed to happen :/
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article 
+                <span className="blue_gradient"> Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
