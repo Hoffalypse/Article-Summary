d86c7b7e1483988fe3ac05cea80b363ec9e6b598
Bryan W <bhoff1980@gmail.com>
2023-04-23 00:40:51 -0600

    .env file exposed was not listed in gitignore

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,52 +1,88 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from "react"
-import { copy, linkIcon, loader, tick} from '../assets'
-import { useLazyGetSummaryQuery } from "../services/article"
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url:"",
-    summary:"",
-  })
-  const [allArticles, setAllArticles] = useState([])
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
 
-  const [getSummary, { error, isFetching}] = useLazyGetSummaryQuery();
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-      const { data } = await getSummary({articleUrl:article.url})
-      if(data?.summary) {
-        const newArticle = { ...article, summary: data.summary}
-        const updateArticles = [newArticle, ...allArticles]
-        setAllArticles(updateArticles)
-        setArticle(newArticle)
-      }
-  }
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updateArticles = [newArticle, ...allArticles];
+      setAllArticles(updateArticles);
+      setArticle(newArticle);
+
+      localStorage.setItem("articles", JSON.stringify(updateArticles));
+    }
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-          <img 
-          src={linkIcon}
-          alt="link icon"
-          className="absolute left-0 my-2 ml-3 w-5"
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
-          <input 
-          type="url"
-          placeholder="Enter a URL"
-          value={article.url}
-          onChange={(e) => setArticle({...article, url:e.target.value})}
-          required
-          className="url_input peer"
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
           />
-          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-grey-700">
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-grey-700"
+          >
             Summarize
           </button>
         </form>
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy icon"
+                  className="w-[40%] h-[40%] onject-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium, text-sm truncate">{item.url}</p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display Results */}
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
