6e18d1a6df28af547a5b2c56c12eff34c332b51c
Bryan W <bhoff1980@gmail.com>
2023-04-23 19:47:33 -0600

    added a select paragraphs feature to choose the length of summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,9 +5,6 @@ import { copy, linkIcon, loader, tick } from "../assets";
 // import { useLazyGetSummaryQuery } from "../services/article";
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-
-
-
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
@@ -16,8 +13,9 @@ const Demo = () => {
   const [allArticles, setAllArticles] = useState([]);
   // const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
   const [loading, setLoading] = useState(false);
-  const[error, setError] = useState()
+  const [error, setError] = useState();
   const [copied, setCopied] = useState("");
+  const [paragraph, setParagraph] = useState(1);
 
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
@@ -33,49 +31,51 @@ const Demo = () => {
     e.preventDefault();
 
     const options = {
-      method: 'GET',
-      url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-      params: {url: article.url, length: '3'},
+      method: "GET",
+      url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+      params: { url: article.url, length: paragraph },
       headers: {
-        'X-RapidAPI-Key': rapidApiKey,
-        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-      }
+        "X-RapidAPI-Key": rapidApiKey,
+        "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+      },
     };
-    setLoading(true)
+    setLoading(true);
     const existingArticle = allArticles.find(
       (item) => item.url === article.url
     );
 
     if (existingArticle) return setArticle(existingArticle);
 
-     axios.request(options)
-    .then(function (response) {
-     
-      if (response.data.summary) {
-        const newArticle = { ...article, summary: response.data.summary };
-        const updatedAllArticles = [newArticle, ...allArticles];
-  
-        // update state and local storage
-        setArticle(newArticle);
-        setAllArticles(updatedAllArticles);
-        localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-      }
-      setLoading(false)
-    }).catch(function (err) {
-      console.error(err);
-      setError(err)
-      setLoading(false)
-    });
+    axios
+      .request(options)
+      .then(function (response) {
+        if (response.data.summary) {
+          const newArticle = { ...article, summary: response.data.summary };
+          const updatedAllArticles = [newArticle, ...allArticles];
 
-  
-
-    
+          // update state and local storage
+          setArticle(newArticle);
+          setAllArticles(updatedAllArticles);
+          localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+        }
+        setLoading(false);
+      })
+      .catch(function (err) {
+        console.error(err);
+        setError(err);
+        setLoading(false);
+      });
   };
+
+  const handleParaChange = (e) => {
+    const {  value } = e.target;
+    setParagraph(value)
+  }
 
   const handleCopy = (copyUrl) => {
     setCopied(copyUrl);
     navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000)
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -85,26 +85,37 @@ const Demo = () => {
           className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
-          <img
-            src={linkIcon}
-            alt="link icon"
-            className="absolute left-0 my-2 ml-3 w-5"
-          />
-          <input
-            type="url"
-            placeholder="Enter a URL"
-            value={article.url}
-            onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            required
-            className="url_input peer"
-          />
-          <button
-            type="submit"
-            className="submit_btn peer-focus:border-gray-700 peer-focus:text-grey-700"
-          >
-            Summarize
-          </button>
+         
+            <img
+              src={linkIcon}
+              alt="link icon"
+              className="absolute left-0 my-2 ml-3 w-5"
+            />
+            <input
+              type="url"
+              placeholder="Enter a URL"
+              value={article.url}
+              onChange={(e) => setArticle({ ...article, url: e.target.value })}
+              required
+              className="url_input peer"
+            />
+            <button
+              type="submit"
+              className="submit_btn peer-focus:border-gray-700 peer-focus:text-grey-700"
+            >
+              Summarize
+            </button>
+          
         </form>
+        <div className="flex justify-center">
+        <label>Summary Paragraphs: &nbsp; &nbsp;</label>
+        <select id="cars" name="cars" className="w-12 rounded shadow-lg border-gray-200" onChange={handleParaChange}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        </div>
         {/* Browse URL History */}
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.map((item, index) => (
@@ -113,7 +124,7 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className="link_card"
             >
-              <div className="copy_btn" onClick={() => handleCopy(item.url) }>
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
                   alt="copy icon"
@@ -131,8 +142,7 @@ const Demo = () => {
       <div className="my-10 max-w-full flex justify-center items-center">
         {loading ? (
           <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
-        ) 
-        : error ? (
+        ) : error ? (
           <p className="font-inter font-bold text-black text-center">
             Well, that was not supposed to happen :/
             <br />
@@ -140,12 +150,11 @@ const Demo = () => {
               {error?.data.error}
             </span>
           </p>
-        ) 
-        : (
+        ) : (
           article.summary && (
             <div className="flex flex-col gap-3">
               <h2 className="font-satoshi font-bold text-gray-600 text-xl">
-                Article 
+                Article
                 <span className="blue_gradient"> Summary</span>
               </h2>
               <div className="summary_box">
